7123a820f13a194b9213d1f9afb4d8a5caed3510
ssgand <sstevegandhi@gmail.com>
2022-11-17 01:41:06 +0000

    recent commits

--- is_chain.c
+++ is_chain.c
@@ -125,4 +125,28 @@ int replace_vars(info_t *info)
 			replace_string(&(info->argv[i]),
 				_strdup(convert_number(getpid(), 10, 0)));
 			continue;
-
+		}
+		node = node_starts_with(info->env, &info->argv[i][1], '=');
+		if (node)
+		{
+			replace_string(&(info->argv[i]),
+					_strdup(_strchr(node->str, '=') + 1));
+			continue;
+		}
+		replace_string(&info->argv[i], _strdup(""));
+	}
+	return (0);
+}
+/**
+ * replace_string - function to replace a string
+ * @old: address of old string
+ * @new: new string
+ *
+ * Return: 1 if replaced, 0 otherwise
+ */
+int replace_string(char **old, char *new)
+{
+	free(*old);
+	*old = new;
+	return (1);
+}
